fcaa9115cd619b84d4b9cbd070900ada86467a47
Vitaliy Shapiro <shapirovet@gmail.com>
2020-11-13 11:44:26 +0300

    add field userDao in UserServiceImpl

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
@@ -7,33 +8,41 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    private UserDao userDao = new UserDaoHibernateImpl();
+
     public void createUsersTable() {
-        new UserDaoHibernateImpl().createUsersTable();
+        userDao.createUsersTable();
+//        new UserDaoHibernateImpl().createUsersTable();
 //        new UserDaoJDBCImpl().createUsersTable();
     }
 
     public void dropUsersTable() {
-        new UserDaoHibernateImpl().dropUsersTable();
+        userDao.dropUsersTable();
+//        new UserDaoHibernateImpl().dropUsersTable();
 //        new UserDaoJDBCImpl().dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        new UserDaoHibernateImpl().saveUser(name, lastName, age);
+        userDao.saveUser(name, lastName, age);
+//        new UserDaoHibernateImpl().saveUser(name, lastName, age);
 //        new UserDaoJDBCImpl().saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        new UserDaoHibernateImpl().removeUserById(id);
+        userDao.removeUserById(id);
+//        new UserDaoHibernateImpl().removeUserById(id);
 //        new UserDaoJDBCImpl().removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return new UserDaoHibernateImpl().getAllUsers();
+        return userDao.getAllUsers();
+//        return new UserDaoHibernateImpl().getAllUsers();
 //        return new UserDaoJDBCImpl().getAllUsers();
     }
 
     public void cleanUsersTable() {
-        new UserDaoHibernateImpl().cleanUsersTable();
+        userDao.cleanUsersTable();
+//        new UserDaoHibernateImpl().cleanUsersTable();
 //        new UserDaoJDBCImpl().cleanUsersTable();
     }
 }
